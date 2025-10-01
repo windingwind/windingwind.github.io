1947de79184b464b17e617fc13a6bbd1c15dc81d
windingwind <33902321+windingwind@users.noreply.github.com>
2025-10-01 14:28:09 +0200

    add: neuralpvs

--- pubs.config.js
+++ pubs.config.js
@@ -1,5 +1,21 @@
 const PUBLICATIONS = [
   {
+    title: "NeuralPVS: Learned Estimation of Potentially Visible Sets",
+    authors: "Xiangyu Wang, Thomas Köhler, Jun Lin Qiu, Shohei Mori, Markus Steinberger, Dieter Schmalstieg",
+    image: "neuralpvs/static/images/representative-tiny.png",
+    conference: "ACM SIGGRAPH Asia 2025",
+    links: {
+      "project page": "neuralpvs/index.html",
+      arxiv: "https://arxiv.org/abs/2509.24677",
+      pdf: "https://arxiv.org/pdf/2509.24677",
+      code: "",
+      video: "neuralpvs/static/videos/supp.mp4",
+      supp: "",
+    },
+    badges: [],
+    comments: `Real-time visibility determination in expansive or dynamically changing environments has long posed a significant challenge in computer graphics. Existing techniques are computationally expensive and often applied as a precomputation step on a static scene. We present NeuralPVS, the first deep-learning approach for visibility computation that efficiently determines from-region visibility in a large scene, running at approximately 100 Hz processing with less than 1% missing geometry. This approach is possible by using a neural network operating on a voxelized representation of the scene. The network's performance is achieved by combining sparse convolution with a 3D volume-preserving interleaving for data compression. Moreover, we introduce a novel repulsive visibility loss that can effectively guide the network to converge to the correct data distribution. This loss provides enhanced robustness and generalization to unseen scenes. Our results demonstrate that NeuralPVS outperforms existing methods in terms of both accuracy and efficiency, making it a promising solution for real-time visibility computation.\nComments: SIGGRAPH Asia 2025\nSubjects: Graphics (cs.GR)\nACM classes: I.3.7\nCite as: arXiv:2509.24677 [cs.GR] (or arXiv:2509.24677v1 [cs.GR] for this version)`
+  },
+  {
     title:
       "Seal-3D: Interactive Pixel-Level Editing for Neural Radiance Fields",
     authors:
@@ -7,11 +23,11 @@ const PUBLICATIONS = [
     image: "https://github.com/windingwind/windingwind.github.io/assets/33902321/084befa0-93f0-47a4-a0da-e310f035119a",
     conference: "ICCV 2023",
     links: {
-      "project page": "https://windingwind.github.io/seal-3d/",
+      "project page": "seal-3d/index.html",
+      paper: "https://openaccess.thecvf.com/content/ICCV2023/html/Wang_Seal-3D_Interactive_Pixel-Level_Editing_for_Neural_Radiance_Fields_ICCV_2023_paper.html",
       arxiv: "https://arxiv.org/abs/2307.15131",
       pdf: "https://arxiv.org/pdf/2307.15131",
-      supp: "",
-      video: "https://windingwind.github.io/seal-3d/static/videos/supp.mp4",
+      video: "seal-3d/static/videos/supp.mp4",
       code: "https://github.com/windingwind/seal-3d/",
     },
     badges: [],
