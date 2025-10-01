b3b8b24188c7e1851d4c015d12cc0fc7843c4e07
windingwind <33902321+windingwind@users.noreply.github.com>
2025-10-01 13:08:16 +0200

    chore: refactor index

---
+++ static/js/pubs.config.js
@@ -0,0 +1,99 @@
+const PUBLICATIONS = [
+  {
+    title:
+      "Seal-3D: Interactive Pixel-Level Editing for Neural Radiance Fields",
+    authors:
+      "Xiangyu Wang, Jingsen Zhu, Qi Ye, Yuchi Huo, Yunlong Ran, Zhihua Zhong, Jiming Chen",
+    image: "https://github.com/windingwind/windingwind.github.io/assets/33902321/084befa0-93f0-47a4-a0da-e310f035119a",
+    conference: "ICCV 2023",
+    links: {
+      "project page": "https://windingwind.github.io/seal-3d/",
+      arxiv: "https://arxiv.org/abs/2307.15131",
+      pdf: "https://arxiv.org/pdf/2307.15131",
+      supp: "",
+      video: "https://windingwind.github.io/seal-3d/static/videos/supp.mp4",
+      code: "https://github.com/windingwind/seal-3d/",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "AdaptiveFusion: Adaptive Multi-Modal Multi-View Fusion for 3D Human Body Reconstruction",
+    authors:
+      "Anjun Chen, Xiangyu Wang, Zhi Xu, Kun Shi, Yan Qin, Yuchi Huo",
+    // image: "https://chen3110.github.io/ImmFusion/resources/framework.png",
+    conference: "IEEE Transactions on Multimedia 2025",
+    links: {
+      "project page": "",
+      paper: "https://doi.org/10.1109/TMM.2025.3613111",
+      arxiv: "https://arxiv.org/abs/2409.04851",
+      pdf: "",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "Towards weather-robust 3D human body reconstruction: Millimeter-wave radar-based dataset, benchmark, and multi-modal fusion",
+    authors:
+      "Anjun Chen, Xiangyu Wang, Kun Shi, Yuchi Huo, Jiming Chen, Qi Ye",
+    // image: "https://chen3110.github.io/ImmFusion/resources/framework.png",
+    conference: "IEEE Transactions on Circuits and Systems for Video Technology 2024",
+    links: {
+      "project page": "",
+      paper: "https://doi.org/10.1109/TCSVT.2024.3461960",
+      arxiv: "",
+      pdf: "",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "ImmFusion: Robust mmWave-RGB Fusion for 3D Human Body Reconstruction in All Weather Conditions",
+    authors:
+      "Anjun Chen, Xiangyu Wang, Kun Shi, Shaohao Zhu, Bin Fang, Yingfeng Chen, Jiming Chen, Yuchi Huo, Qi Ye",
+    image: "https://chen3110.github.io/ImmFusion/resources/good_case.png",
+    conference: "ICRA 2023",
+    links: {
+      "project page": "https://chen3110.github.io/ImmFusion/index.html",
+      paper: "",
+      arxiv: "https://arxiv.org/abs/2210.01346",
+      pdf: "https://arxiv.org/pdf/2210.01346",
+      supp: "",
+      video: "",
+      code: "https://github.com/Chen3110/ImmFusion",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "mmBody Benchmark: 3D Body Reconstruction Dataset and Analysis for Millimeter Wave Radar",
+    authors:
+      "Anjun Chen, Xiangyu Wang, Shaohao Zhu, Yanxu Li, Jiming Chen, Qi Ye",
+    image: "https://chen3110.github.io/mmbody/resources/good_case.png",
+    conference: "ACM MM 2022",
+    links: {
+      "project page": "https://chen3110.github.io/mmbody/",
+      paper: "https://dl.acm.org/doi/10.1145/3503161.3548262",
+      arxiv: "https://arxiv.org/abs/2209.05070",
+      pdf: "https://arxiv.org/pdf/2209.05070",
+      supp: "",
+      video: "",
+      code: "",
+      data: "https://github.com/Chen3110/mmBody",
+    },
+    badges: [],
+  },
+];
+
+const HIGHLIGHT_NAME = "xiangyu wang";
+
+window._PUB_CONFIG = {
+  highlightName: HIGHLIGHT_NAME,
+  publications: PUBLICATIONS,
+};
